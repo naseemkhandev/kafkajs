9f70d42092ae1b5c84d343126978179ee7184d55
Naseem Khan <devnaseemkhan@gmail.com>
2025-04-02 16:54:25 +0500

    add Kafka consumer integration to handle incoming messages

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import "dotenv/config";
 import cors from "cors";
 import { connectKafkaProducer } from "./config/kafka.config.js";
+import { consumeMessage } from "./helpers/index.js";
 
 const PORT = process.env.PORT || 8080;
 const app: Application = express();
@@ -19,6 +20,11 @@ app.get("/", (req: Request, res: Response) => {
 // * Kafka Producer
 connectKafkaProducer().catch((error) => {
   console.error("Error connecting Kafka Producer", error);
+});
+
+// * Kafka Consumer
+consumeMessage(process.env.KAFKA_TOPIC as string).catch((error) => {
+  console.error("Error consuming message", error);
 });
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
