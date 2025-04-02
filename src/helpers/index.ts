4d234822507521493fdac45f2ce2c5be5ffaf847
Naseem Khan <devnaseemkhan@gmail.com>
2025-04-02 16:45:56 +0500

    add message production and consumption helpers for Kafka integration

---
+++ src/helpers/index.ts
@@ -0,0 +1,36 @@
+import { consumer, producer } from "../config/kafka.config.js";
+
+export const produceMessage = async (topic: string, message: string) => {
+  try {
+    const result = await producer.send({
+      topic,
+      messages: [{ value: JSON.stringify(message) }],
+    });
+
+    console.log("Message produced:", result);
+    await producer.disconnect();
+  } catch (error) {
+    console.error("Error producing message:", error);
+    throw new Error("Failed to produce message");
+  }
+};
+
+export const consumeMessage = async (topic: string) => {
+  try {
+    await consumer.connect();
+    await consumer.subscribe({ topic, fromBeginning: true });
+
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        console.log({
+          partition,
+          offset: message.offset,
+          value: message.value.toString(),
+        });
+      },
+    });
+  } catch (error) {
+    console.error("Error consuming message:", error);
+    throw new Error("Failed to consume message");
+  }
+};
