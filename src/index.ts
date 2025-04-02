757941bd7a5f8b05ae748ace70b0a43d24b7705f
Naseem Khan <devnaseemkhan@gmail.com>
2025-04-02 12:17:46 +0500

    update server response message and change default port to 8080

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import express, { Application, Request, Response } from "express";
 import "dotenv/config";
 import cors from "cors";
-const app: Application = express();
-const PORT = process.env.PORT || 7000;
 
-// * Middleware
+const PORT = process.env.PORT || 8080;
+const app: Application = express();
+
+// * Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// * Routes
 app.get("/", (req: Request, res: Response) => {
-  return res.send("It's working 🙌");
+  return res.send("<h1>Server is Running 🚀</h1>");
 });
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
